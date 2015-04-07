ff2ef729d6ef5c0733d20f8deaf7fdfa87db7562
Subodh Yadav <subodh29031995@gmail.com>
2015-04-07 15:05:40 +0530

    finished milestone 1

---
+++ app/src/androidTest/java/project/is/joggler/ApplicationTest.java
@@ -0,0 +1,17 @@
+package project.is.joggler;
+
+import android.app.Activity;
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    //Activity mActivity = this.getActivity();
+    public ApplicationTest() {
+        super(Application.class);
+    }
+
+
+}
